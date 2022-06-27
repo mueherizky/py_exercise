5b73d1a4729637295b70f141d081ea41ec4afa47
mueherizky <102344789+mueherizky@users.noreply.github.com>
2022-06-28 06:18:17 +0700

    number 1

---
+++ coding_exercise.py
@@ -0,0 +1,10 @@
+########## 1 ###########
+
+# make function
+def max_func(*numbers):
+    print(f"This numbers what you input {numbers}")
+    max_num = max(numbers)  
+    print(max_num)
+
+# call function
+max_func(1,2,4,7,100)
