d1df101a9b3cc2f6144e66e278fb39b926513cdf
mueherizky <102344789+mueherizky@users.noreply.github.com>
2022-06-28 06:32:11 +0700

    add number 2

--- coding_exercise.py
+++ coding_exercise.py
@@ -8,3 +8,18 @@ def max_func(*numbers):
 
 # call function
 max_func(1,2,4,7,100)
+
+
+########## 2 ###########
+def fizz_buzz(number):
+    if (number % 3 == 0) and (number % 5 == 0):
+        print("FizzBuzz")
+    elif number % 5 == 0:
+        print("Buzz") 
+    elif number % 3 == 0:
+        print("Fizz")
+    else:
+        print(number)
+
+fizz_buzz(20)
+
