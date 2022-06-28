e4e71d9f9e7b75dbc517bf60ad3fba63c1c086fb
mueherizky <102344789+mueherizky@users.noreply.github.com>
2022-06-28 07:10:16 +0700

    make more readable and fix code for correct output

--- coding_exercise.py
+++ coding_exercise.py
@@ -40,15 +40,16 @@ def showNumbers(limit):
 showNumbers(20)
 
 
-
 ########## 5 ###########
 
 
 
 ########## 6 ###########
 def show_stars(rows):
-    for stars in range(1,rows):
+    for stars in range(1,rows+1):
         many_stars = "*" * stars
         print(many_stars)
 
 show_stars(10)
+
+
