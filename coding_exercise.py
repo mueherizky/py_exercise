9d1284190ca7f6469ca52166821496345bdbb759
mueherizky <102344789+mueherizky@users.noreply.github.com>
2022-06-28 15:23:18 +0700

    clean code and add number 7

--- coding_exercise.py
+++ coding_exercise.py
@@ -3,11 +3,12 @@
 # make function
 def max_func(*numbers):
     print(f"This numbers what you input {numbers}")
-    max_num = max(numbers)  
+    max_num = max(numbers)
     print(max_num)
 
+
 # call function
-max_func(1,2,4,7,100)
+max_func(1, 2, 4, 7, 100)
 
 
 ########## 2 ###########
@@ -15,11 +16,12 @@ def fizz_buzz(number):
     if (number % 3 == 0) and (number % 5 == 0):
         print("FizzBuzz")
     elif number % 5 == 0:
-        print("Buzz") 
+        print("Buzz")
     elif number % 3 == 0:
         print("Fizz")
     else:
         print(number)
+
 
 fizz_buzz(20)
 
@@ -27,14 +29,14 @@ fizz_buzz(20)
 ########## 3 ###########
 
 
-
 ########## 4 ###########
 def showNumbers(limit):
-    for number in range(1,limit+1):
+    for number in range(1, limit+1):
         if number % 2 == 0:
             print(f"{number} is even number")
         else:
             print(f"{number} is odd number")
+
 
 showNumbers(20)
 
@@ -42,13 +44,25 @@ showNumbers(20)
 ########## 5 ###########
 
 
-
 ########## 6 ###########
 def show_stars(rows):
-    for stars in range(1,rows+1):
+    for stars in range(1, rows+1):
         many_stars = "*" * stars
         print(many_stars)
+
 
 show_stars(10)
 
 
+########## 7 ###########
+def prime_num(limit):
+    for number in range(1, limit+1):
+        if number > 1:
+            for num in range(2, number):
+                if number % num == 0:
+                    break
+            else:
+                print(number)
+
+
+prime_num(20)
